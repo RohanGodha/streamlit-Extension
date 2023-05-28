db58ff65810b0abff3c3344077819267bb8dbad2
Rohan Godha <yoyorodha@gmail.com>
2023-05-28 21:14:30 +0530

    Create main.py

---
+++ dailyScheduler/main.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import random
+
+# Title of the app
+st.title("Daily Schedule")
+
+# Define the schedule
+schedule = [
+    ("05:00 - 06:00", "Wake Up, Warm Water, Brush, Bath, Drink 500mL Milk+Dryfruits+Amla, YT(Playlist)"),
+    ("06:00 AM - 08:30 AM", "Study"),
+    ("08:30 AM - 05:00 PM", "College/Lunch/Self Study"),
+    ("05:00 PM - 06:00 PM", "1 Hour of Sleep (Yoga Nindra 3rd Eye Opener)"),
+    ("06:00 PM - 08:30 PM", "Milk Buying, Dinner, Home Conversation"),
+    ("08:30 PM - 11:30 PM", "Study (Preferred: DSA)"),
+    ("11:30 PM - 01:30 AM", "Study + 250 mL Milk with Haldi/Plain/Coffee/Chai"),
+    ("01:30 AM - 05:00 AM", "Sleep (Yoga Nindra 3rd Eye Opener)")
+]
+
+# Custom CSS styles
+css = """
+    <style>
+        .schedule {
+            font-weight: bold;
+            font-style: italic;
+            color: #ffffff;
+            padding: 10px;
+            margin-bottom: 10px;
+            border-radius: 5px;
+            background-size: cover;
+            background-position: center;
+        }
+    </style>
+"""
+st.markdown(css, unsafe_allow_html=True)
+
+# List of background images
+background_images = ["image1.jpeg", "image2.jpeg", "image3.jpeg", "image4.jpeg"]
+
+# Create checkboxes for each activity
+for time_slot, activity in schedule:
+    activity_checked = st.checkbox(label=f"{time_slot}: {activity}")
+    if activity_checked:
+        edited_activity = st.text_input(label="Edit activity:", value=activity)
+        if edited_activity:
+            activity = edited_activity
+    random_image = random.choice(background_images)
+    activity_style = f'<div class="schedule" style="background-image: url(\'{random_image}\');">{time_slot}: {activity}</div>'
+    st.markdown(activity_style, unsafe_allow_html=True)
