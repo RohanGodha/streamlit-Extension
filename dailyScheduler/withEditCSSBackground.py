7f66318a2f110954608b087ee08987a9bad54150
Rohan Godha <yoyorodha@gmail.com>
2023-05-28 21:13:56 +0530

    Create withEditCSSBackground.py

---
+++ dailyScheduler/withEditCSSBackground.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+# Title of the app
+st.title("Daily Schedule")
+
+# Define the schedule
+schedule = [
+    ("05:00 - 06:00", "Wake Up, Warm Water, Brush, Bath, Drink 500mL Milk+Badaam+Akhroat+Amla, YT(Playlist)"),
+    ("06:00 - 08:30", "Study"),
+    ("08:30 - 05:00", "College/Lunch/Self Study"),
+    ("05:00 - 06:00", "1 Hour of Sleep (Yoga Nindra 3rd Eye Opener)"),
+    ("06:00 - 08:30", "Milk Buying, Dinner, Home Conversation"),
+    ("08:30 - 11:30", "Study (Preferred: DSA)"),
+    ("11:30 - 01:30", "Study + 250 mL Milk with Haldi/Plain/Coffee/Chai"),
+    ("01:30 - 05:00", "Sleep (Yoga Nindra 3rd Eye Opener)")
+]
+
+# Custom CSS styles
+css = """
+    <style>
+        .schedule {
+            font-weight: bold;
+            font-style: italic;
+            background-color: #f1f1f1;
+            color: #333333;
+            padding: 10px;
+            margin-bottom: 10px;
+            border-radius: 5px;
+            background-image: url('background.jpg');
+            background-size: cover;
+            background-position: center;
+        }
+    </style>
+"""
+st.markdown(css, unsafe_allow_html=True)
+
+# Create checkboxes for each activity
+for time_slot, activity in schedule:
+    activity_checked = st.checkbox(label=f"{time_slot}: {activity}")
+    if activity_checked:
+        edited_activity = st.text_input(label="Edit activity:", value=activity)
+        if edited_activity:
+            activity = edited_activity
+    st.markdown(f'<div class="schedule">{time_slot}: {activity}</div>', unsafe_allow_html=True)
